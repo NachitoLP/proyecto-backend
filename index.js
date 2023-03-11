37d34b4b748f9b87a136508e61fc4af3365bcafe
Joaquin <joaquinacholopez@gmail.com>
2023-03-11 12:52:56 -0300

    Desafio entregable 2

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
-class ProductManager {
-    constructor(){
-        this.products = []
-    }
-    getProducts = () => {
-        return this.products;
-    }
-    addProduct = (nombre, descripcion, precio, imagen, stock, id) => {
-        const product = {
-            nombre,
-            descripcion,
-            precio,
-            imagen,
-            id,
-            stock
-        }
-        if (this.products.length === 0) {
-            product.id = 1;
-        }
-        else {
-            product.id = this.products[this.products.length-1].id + 1;
-        }
-        this.products.push(product);
-    }
-    getProductsById = (id) => {
-        let productoEncontrado = this.products.find(producto => producto.id === id)
-        if (productoEncontrado){
-            console.log(productoEncontrado)
-        }else{
-            console.log("producto no encontrado");
-        }
-    }
-}
+const { ProductManager } = require("./productManager");
 
-const manejadorProductos = new ProductManager();
-manejadorProductos.addProduct("Manzana", "Una rica manzana", 5000, "*", 20);
-manejadorProductos.addProduct("Banana", "Una rica banana", 2000, "+", 10);
-manejadorProductos.addProduct("Ananá", "Una rica ananá", 3000, "+", 2);
-manejadorProductos.addProduct("Frutilla", "Una rica frutilla", 500, "+", 15);
-console.log(manejadorProductos.getProducts());
-manejadorProductos.getProductsById(3);
-manejadorProductos.getProductsById(6);
+const product = new ProductManager()
+
+product.getProducts()
+.then(resp => console.log(resp));
+
+product.addProduct({
+    name: "Ananá",
+    description: "Una rica ananá",
+    price: 2000,
+    href: "Sin imagen",
+    code: "123456",
+    stock: 10
+})
+
+product.deleteProductsById(2);
+
+product.getProductsById(4)
+
+product.updateProduct(2, 5000);
---
+++ productManager.js
@@ -0,0 +1,91 @@
+const fs = require("fs");
+const route = "./archivos/productos.json"
+
+class ProductManager {
+    constructor(){
+        this.route = route
+    }
+    getProducts = async () => {
+        try {
+            if(fs.existsSync(this.route)) {
+                const products = await fs.promises.readFile(this.route);
+                return JSON.parse(products);
+            }
+            else{
+                return []
+            }
+        } catch (error) {
+            return []
+        }
+    }
+
+    addProduct = async ( product ) => {
+        const products = await this.getProducts()
+
+        if (products.length === 0) {
+            product.id = 1;
+        }
+        else {
+            product.id = products[products.length-1].id + 1;
+        }
+        
+        products.push(product);
+        await fs.promises.writeFile(route,JSON.stringify(products));
+        return products;
+    }
+
+
+    getProductsById = async ( id ) => {
+        const products = await this.getProducts();
+
+        let productFound = products.find(product => product.id === id)
+        if (productFound){
+            console.log(productFound)
+        }else{
+            console.log("producto no encontrado");
+        }
+    }
+
+    updateProduct = async ( id , change ) => {
+        const products = await this.getProducts();
+
+        let productFound = products.find(product => product.id === id)
+        let productDeleted = products.find(product => product.id === id)
+        let position = products.indexOf(productDeleted)
+
+
+        if (productFound){
+            productFound.price = change;
+
+            products.splice(position,1);
+            products.splice(position,0,productFound);
+
+            await fs.promises.writeFile(route,JSON.stringify(products));
+            return products;
+        }
+        else {
+            console.log("producto no encontrado");
+        }
+
+    }
+
+    deleteProductsById = async ( id ) => {
+        const products = await this.getProducts();
+
+        let productFound = products.find(product => product.id === id)
+        let position = products.indexOf(productFound)
+
+        if (position != -1){
+            products.splice(position,1)
+            await fs.promises.writeFile(route,JSON.stringify(products));
+            return products;
+        }
+        else {
+            console.log("producto no encontrado en la lista");
+        }
+    }
+}
+
+module.exports = {
+    ProductManager
+}
